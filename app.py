f101e3e6cd3dc250aacbd8012deec85d5a804e8d
Mg <gamem.8818@gmail.com>
2015-10-29 22:16:17 -0400

    cleanup code

--- app.py
+++ app.py
@@ -1,30 +1,10 @@
 from flask import Flask
-from flask import stream_with_context, make_response, request, Response
+from flask import request
 from flask import jsonify
-from flask import g
-import collections
-import requests
+import os
 import mergelist
 
-__author__ = 'mmgu'
-
-
-stream1 = [1, 2, 3, 4, 5]
-stream2 = [2, 3, 4, 4, 5]
-stream3 = [3, 4, 5]
-
 app = Flask(__name__)
-
-
-class TestData:
-    def __init__(self):
-        self.current = 0
-        self.last = None
-
-t = TestData()
-t2 = TestData()
-t3 = TestData()
-
 merge_list = mergelist.MergeList()
 
 
@@ -35,40 +15,5 @@ def merge():
     return jsonify(merge_list.start())
 
 
-@app.route('/get/<stream_name>')
-def get_stream(stream_name):
-    #response = {"last": last, "current": current, "stream": stream_name}
-    # last = current
-    # print(current)
-    # print(last)
-    if stream_name == "a":
-        response = generate_response(stream_name)
-    elif stream_name == 'b':
-        response = generate_response3(stream_name)
-    else:
-        response = generate_response2(stream_name)
-    return jsonify(response)
-
-def generate_response(stream_name):
-    res = {"last":t.last, "current": stream1[t.current], "stream": stream_name}
-    t.last = t.current
-    t.current += 1
-    print(res)
-    return res
-
-def generate_response2(stream_name):
-    res = {"last":t2.last, "current": stream2[t2.current], "stream": stream_name}
-    t2.last = t2.current
-    t2.current += 1
-    print(res)
-    return res
-
-def generate_response3(stream_name):
-    res = {"last":t3.last, "current": stream3[t3.current], "stream": stream_name}
-    t3.last = t3.current
-    t3.current += 1
-    print(res)
-    return res
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=int(os.environ.get('PORT', 3000)))
--- mergelist.py
+++ mergelist.py
@@ -2,8 +2,6 @@ import heapq
 import requests
 import json
 
-__author__ = 'mmgu'
-
 
 class MergeList:
     def __init__(self, name1=None, name2=None):
@@ -12,91 +10,33 @@ class MergeList:
         self.cache = []
         self.service_endpoint = "https://api.pelotoncycle.com/quiz/next/"
         self.last_value = None
-        self.stream_list = []
 
-
+    # send one get request to get one data from the stream
     def get_stream_data(self, stream_name):
         try:
             res = requests.get(self.service_endpoint + stream_name, headers={"Accept": "application/json"})
             res = res.json()
         except json.JSONDecodeError:
+            print("Json Decode Error")
             return
-
         heapq.heappush(self.cache, (res["current"], res["stream"]))
-        self.stream_list.append(stream_name)
 
-    def get_last_value(self, num = None):
-        return num
-
+    # pop the smallest one from cache
     def get_smallest(self):
-        #print(self.last_value)
         try:
             key, value = heapq.heappop(self.cache)
         except IndexError:
-            return {"last": self.last_value, "current": None,"stream": None}
-        response = {"last": self.last_value, "current": key, "stream": value}
+            print("Stream drained")
+            return {"last": self.last_value, "current": None}
+        response = {"last": self.last_value, "current": key}
         self.last_value = str(key)
-        #print(self.last_value)
-        self.stream_list.remove(value)
-        # print(self.cache)
-        # print(key)
-        # print(value)
-        # print(self.last_value)
-        # print(self.stream_list)
         return response
 
-    def cleanup_old_stream(self, new_stream_name, old_stream_name):
-        for i,v in self.cache:
-            if v == old_stream_name:
-                self.cache.remove((i,v))
-        if old_stream_name in self.stream_list:
-            self.stream_list.remove(old_stream_name)
-        return new_stream_name
-
-    def start(self, name1=None, name2=None):
-        # if name1 and name1 != self.stream1_name:
-        #     self.stream1_name = self.cleanup_old_stream(name1,self.stream1_name)
-        # if name2 and name2 != self.stream2_name:
-        #     self.stream2_name = self.cleanup_old_stream(name2,self.stream2_name)
+    # entry function
+    def start(self):
         self.get_stream_data(self.stream1_name)
         self.get_stream_data(self.stream2_name)
         return self.get_smallest()
-        # if not self.cache:
-        #     print("drained")
-        #     print(self.stream_list)
-        #     self.get_stream_data(self.stream1_name)
-        #     self.get_stream_data(self.stream2_name)
-        #     return self.get_smallest()
-        # elif self.stream1_name not in self.stream_list:
-        #     self.get_stream_data(self.stream1_name)
-        # elif self.stream2_name not in self.stream_list:
-        #     self.get_stream_data(self.stream2_name)
-        # return self.get_smallest()
 
-# stream1 = [1, 2, 3, 4, 5]
-# stream2 = [2, 3, 4, 4, 5]
-# #stream3 = [3, 4, 5]
-# print(sorted(stream1+stream2))
-# test = MergeList("stream1","stream2")
-# # print(test.start())
-# # print(test.start())
-# # print(test.start())
-# # print(test.start())
-# # print(test.start())
-# #test.stream1_name = "b"
-# # print(sorted(stream2+stream3))
-# # print(test.start(name1="b"))
-# # print(test.start())
-# # print(test.start())
-# # print(test.start())
-# print(test.start())
-# print(test.start())
-# #
-# print(test.start())
-# print(test.start())
-# test.get_stream_data("a")
-# print(test.last_value)
-# test.get_stream_data("b")
-# print(test.last_value)
-# print(test.get_smallest())
-# print(test.get_smallest())
+
+
